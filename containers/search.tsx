5dc87ef9ed26498b47d99358eeb3d454f1732fcb
andrestrw <133chicagobulls@gmail.com>
2024-01-17 06:15:42 +0100

    Comienzo maquetación

--- containers/search.tsx
+++ containers/search.tsx
@@ -1,17 +1,24 @@
-"use server";
+"use client";
 
-import { search } from "@/app/actions";
+import { SendOutlined } from "@ant-design/icons";
 import { useURL } from "@/hooks";
 
 const Search = () => {
+  const { setQuery } = useURL();
+
+  const onSubmit = (e: any) => {
+    e.preventDefault();
+    const channel = e.target[0].value;
+    setQuery({ channel, page: 1 });
+  };
   return (
     <main className="container mx-auto pt-14 px-8">
       <div className="flex justify-center ">
         <form
           className="flex flex-col gap-6 w-2/4 justify-center justify-items-center rounded-lg   "
-          action={search}
+          onSubmit={onSubmit}
         >
-          <input className="rounded-lg px-3 py-3 " name="search" />
+          <input className="rounded-lg px-3 py-3 " name="channel" />
 
           <div className="flex justify-center items-center gap-2 border border-white px-3 py-3 rounded-lg">
             <input className="text-white" type="submit" />
